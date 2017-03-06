68c4a5e88b4a19e0c96010465601a57424744f84
Bftech <corentin.monmasson@gmail.com>
2017-03-06 21:25:58 +0100

    update

--- 404.php
+++ 404.php
@@ -3,7 +3,8 @@ get_header();
 ?>
 			<div id="posts">
 				<article class="post">
-					404
+					<h3 class="title404">Erreur <img src="<?php echo get_template_directory_uri();?>/img/404.svg" alt=""></h3>
+					<p>Désolé, cette page n'est pas disponible.</p>
 				</article>
 			</div>
 <?php
